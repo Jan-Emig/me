4d19dceefabf80e4ec2edad68ab087c3df18a941
Jan-Emig <jan.emig@icloud.com>
2022-12-29 20:43:49 +0100

    feat: Added initial fade in animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,50 @@
-import { Box, BoxProps, Center, Flex, FlexProps, Link } from "@chakra-ui/react";
+import { Box, BoxProps, Center, chakra, ChakraProps, Flex, FlexProps, Link, shouldForwardProp } from "@chakra-ui/react";
+import { HTMLMotionProps, isValidMotionProp, motion } from "framer-motion";
 import { Dispatch, FC, SetStateAction, useContext, useState } from "react";
 import { SetShowMouseState } from "../App";
 import MouseContext from "../context";
 
-const NavLink: FC<{ content: string, href: string, noSeparator?: boolean }> = ({ content, href, noSeparator }) => {
+const MotionLink = chakra(motion.a, { shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop) || prop === 'transition'});
+
+const MotionBox = chakra(motion.div, { shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop) || prop === 'transition'});
+
+const NavLink: FC<{ content: string, href: string, fadeInDelay?: number, noSeparator?: boolean }> = ({ content, href, fadeInDelay = 0.2, noSeparator }) => {
     const { toggleVisibility } = useContext(MouseContext);
     return (
-        <>
-            <Link 
+        <MotionBox
+            display='flex'
+            alignItems='center'
+        >
+            <MotionLink 
+                initial={{ opacity: 0, top: '10px' }}
+                whileInView={{ opacity: 1, top: 0 }}
+                transition={{ delay: fadeInDelay } as any}
+                // as={motion.a}
+                pos='relative'
                 href={href}
                 _hover={{
                     color: 'brand.600',
                     opacity: 1
                  }}
-                 opacity={0.6}
-                transition='color, opacity 0.2s ease-in-out'
+                opacity={0.6}
+                // transition='color, opacity 0.2s ease-in-out'
                 onMouseEnter={toggleVisibility}
                 onMouseLeave={toggleVisibility}
-            >{content}</Link>
-            { !noSeparator && <Separator /> }
-        </>
+            >{content}</MotionLink>
+            { !noSeparator && <Separator fadeInDelay={fadeInDelay + 0.4} /> }
+        </MotionBox>
     )
 }
 
-const Separator: FC<FlexProps> = (props) => {
+const Separator: FC<ChakraProps & { fadeInDelay?: number}> = ({ fadeInDelay = 0.4, ...props}) => {
     return (
-        <Flex 
+        <MotionBox
+            position='relative'
+            initial={{ opacity: 0, top: '-10px' }}
+            whileInView={{ opacity: 1, top: 0 }}
+            transition={{ delay: fadeInDelay } as any}
+            display='flex' 
+            alignItems='center'
             mx={5}
             opacity={0.6}
             {...props}
@@ -46,7 +65,7 @@ const Separator: FC<FlexProps> = (props) => {
                 borderRightRadius='1px'
                 bgColor='white'
             />
-        </Flex>
+        </MotionBox>
 
     )
 };
@@ -56,10 +75,10 @@ const Header: FC = () => {
         return (
             <header>
                 <Center mt='40px' color='whiteAlpha.700' fontWeight='bold'>
-                    <NavLink content='Home' href='#' />
-                    <NavLink content='Why Me' href='#' />
-                    <NavLink content='My Work' href='#' />
-                    <NavLink content='Reviews' href='#' noSeparator />
+                    <NavLink content='Home' href='#'  />
+                    <NavLink content='Why Me' href='#' fadeInDelay={0.4} />
+                    <NavLink content='My Work' href='#' fadeInDelay={0.6} />
+                    <NavLink content='Reviews' href='#' fadeInDelay={0.8} noSeparator />
                 </Center>
             </header>
         )
---
+++ src/sections/subsections/HeroRight.tsx
@@ -0,0 +1,132 @@
+import { Box, BoxProps, createIcon, Heading, IconProps } from "@chakra-ui/react";
+import Lottie from "lottie-react";
+import { FC } from "react";
+
+import HexagonMask from '/src/assets/hexagon_mask.svg';
+import FigureLottie from '../../assets/figure_lottie.json';
+
+const skills = [ 'React', 'MySQL', 'Laravel', 'TypeScript' ].sort((a, b) => 0.5 - Math.random());
+
+const HexagonIcon = createIcon({
+    displayName: 'HexagonIcon',
+    viewBox: '0 0 306 354',
+    path: <path d="M149 1.4a8 8 0 0 1 8 0l144.5 84.3a8 8 0 0 1 4 6.9v168.8a8 8 0 0 1-4 7L157 352.5a8 8 0 0 1-8 0L4.5 268.3a8 8 0 0 1-4-6.9V92.6a8 8 0 0 1 4-7L149 1.5Z"/>
+});
+
+const SkillBox: FC<BoxProps & { name: string, size?: 'sm' | 'md' | 'lg'}> = ({name, size = 'md', ...props}) => {
+    const getCompSize = (size: string) => {
+        const sizes = { headingSize: 'md' }
+        if (size === 'sm') return { headingSize: 'sm'};
+        return sizes;
+    }
+    
+    const { headingSize } = getCompSize(size);
+    
+    return (
+        <Box
+            position='absolute'
+            border='1px'
+            borderRadius='lg'
+            borderColor='brand.500'
+            px={5}
+            py={3}
+            fontWeight='bold'
+            color='brand.500'
+            _after={{ 
+                content: '""',
+                position: 'absolute',
+                top: '-5px',
+                left: '-5px',
+                width: 'calc(100% + 10px)',
+                h:'calc(100% + 10px)',
+                border: '1px',
+                borderColor: 'brand.500',
+                borderRadius: '10px',
+                zIndex: -1,
+                opacity: 0.5,
+            }}
+            {...props}
+        >
+            <Heading 
+                position='relative'
+                size={headingSize}
+                _after={{ 
+                    content: '""',
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    width: '110%',
+                    height: '110%',
+                    backgroundColor: 'brand.500',
+                    zIndex: -1,
+                    opacity: 0.3,
+                    filter: 'blur(20px)',
+                 }}
+            >{ name }</Heading>
+        </Box>
+    )
+};
+
+const HexagonBorder: FC<IconProps> = (props) => {
+    return (
+        <HexagonIcon 
+            position='absolute'
+            top='50%'
+            left='50%'
+            transform='translate(-50%, -50%)'
+            fill='transparent' 
+            {...props} 
+        />
+    )
+}
+
+const HeroRight: FC = () => {
+
+    const render = () => {
+        return (
+            <Box 
+                position='relative'
+                mt={{ base: '2rem', hero_sm: '130px'}}
+                w={{ base: '', hero_sx: '230px', hero_sm: 'fit-content' }}
+            >
+                <Box
+                    position='relative'
+                    w='fit-content'
+                    left={{ base: '50%', hero_sm: '0' }}
+                    transform={{ base: 'translateX(-50%) scale(0.6)', hero_sm: 'scale(0.9)', hero_md: 'scale(1)' }}
+                >
+                    <SkillBox name={skills[0]} top='-20px' right='calc(100% - 40px)' />
+                    <SkillBox name={skills[1]} size='sm' top='0px' left='calc(100% - 40px)' />
+                    <SkillBox name={skills[2]} size='sm' bottom='10px' left='-110px' />
+                    <SkillBox name={skills[3]} size='md' bottom='-25px' right='-85px' />
+                    <HexagonIcon
+                        position='absolute'
+                        left='2%'
+                        top='2%'
+                        fill='brand.500'
+                        boxSize='380px'
+                        filter='blur(20px)'
+                        opacity={0.6}
+                    />
+                    <HexagonIcon fill='brand.500' boxSize='380px' />
+                    <HexagonBorder strokeWidth={2} stroke='brand.500' boxSize='400px' />
+                    <HexagonBorder strokeWidth={1} stroke='brand.500' boxSize='415px' />
+                    <Box
+                        position='absolute'
+                        top='-135px'
+                        left='37px'
+                        w='380px'
+                        h='515px'
+                        style={{ maskImage: `url(${HexagonMask})`, WebkitMaskImage: `url(${HexagonMask})`, maskRepeat: 'no-repeat', WebkitMaskRepeat: 'no-repeat' }}
+                        >
+                            <Lottie animationData={FigureLottie} loop autoplay style={{ position: 'absolute', bottom: '-30px', left: '-430px', width: '1000px' }} />    
+                    </Box>
+                </Box>
+            </Box>
+        )
+    }
+
+    return render();
+};
+
+export default HeroRight;
